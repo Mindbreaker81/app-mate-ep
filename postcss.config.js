7ca88d8e9640eee5c20044ed3dbf5ced815f1aea
mindbreaker81 <edmundo.rosales@gmail.com>
2025-10-26 02:10:11 +0200

    test(config): disable tailwind processing during tests

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
+const isTestEnvironment =
+  process.env.NODE_ENV === 'test' ||
+  typeof process.env.VITEST_WORKER_ID !== 'undefined' ||
+  process.env.VITEST === 'true';
+
 export default {
-  plugins: [tailwindcss(), autoprefixer()],
+  plugins: isTestEnvironment ? [] : [tailwindcss(), autoprefixer()],
 };
